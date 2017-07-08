ee14684d6bae241184719cf3afabfd353f866bfd
albertoRamon <a.ramonportoles@gmail.com>
2017-07-08 21:33:32 +0100

    Update to Flink1.2, some clases were moved

--- ReadKylinFromFlink/flink-scala-project/src/main/scala/org/apache/flink/quickstart/WordCount.scala
+++ ReadKylinFromFlink/flink-scala-project/src/main/scala/org/apache/flink/quickstart/WordCount.scala
@@ -20,8 +20,9 @@ package DataSources
 
 import org.apache.flink.api.scala._
 import org.apache.flink.api.java.io.jdbc.JDBCInputFormat
-import org.apache.flink.api.table.Row
-import org.apache.flink.api.table.typeutils.RowTypeInfo
+//import org.apache.flink.api.table.Row (TIP: OLD Package)
+import org.apache.flink.api.java.typeutils.RowTypeInfo
+//import org.apache.flink.api.table.typeutils.RowTypeInfo (TIP: OLD Package)
 import org.apache.flink.api.common.typeinfo.{BasicTypeInfo, TypeInformation}
 import java.util.Date
 
@@ -40,8 +41,11 @@ object WordCount {
     var longColum: TypeInformation[Long] = createTypeInformation[Long]
     var bigDecimalColum: TypeInformation[BigDecimal] = createTypeInformation[BigDecimal]
     var dateColum: TypeInformation[Date] = createTypeInformation[Date]
+    var stringColum: TypeInformation[String] = createTypeInformation[String]
 
-    val DB_ROWTYPE = new RowTypeInfo(Seq(dateColum,bigDecimalColum,longColum))
+    val DB_ROWTYPE = new RowTypeInfo(dateColum,bigDecimalColum,longColum)
+    //val DB_ROWTYPE = new RowTypeInfo(longColum)
+    //val DB_ROWTYPE = new RowTypeInfo(Seq(longColum))    (TIP: OLD Package)
 
     val inputFormat = JDBCInputFormat.buildJDBCInputFormat()
       .setDrivername("org.apache.kylin.jdbc.Driver")
@@ -49,6 +53,7 @@ object WordCount {
       .setUsername("ADMIN")
       .setPassword("KYLIN")
       .setQuery("select part_dt, sum(price) as total_selled, count(distinct seller_id) as sellers from kylin_sales group by part_dt order by part_dt")
+      //.setQuery("select count(distinct seller_id) as sellers from kylin_sales group by part_dt order by part_dt")
       .setRowTypeInfo(DB_ROWTYPE)
       .finish()
 
